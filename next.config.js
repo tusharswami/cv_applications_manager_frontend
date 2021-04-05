20982e208149a64692a56eac2417f9545de16038
tusharswami <tusharswami01@gmail.com>
2021-04-05 13:04:13 +0530

    Card changes

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = withCSS({
         APP_NAME: 'GeoSpoc CV manager',
         API_DEVELOPMENT: 'http://localhost:8000/api',
         API_PRODUCTION: 'https://cv-application-manager.herokuapp.com/api',
-        PRODUCTION: false,
+        PRODUCTION: true,
         // DOMAIN_PRODUCTION: 'https://sharp-shockley-118ffb.netlify.app/',
         DOMAIN_DEVELOPMENT: 'http://localhost:3000',
         GOOGLE_CLIENT_ID:"770856621212-2ogm8kn0p4hv6ispc0upuomnnuilhmkb.apps.googleusercontent.com",
