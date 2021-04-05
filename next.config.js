771daeef010b8b7e381cb013a83b5f6f2966f4ca
tusharswami <tusharswami01@gmail.com>
2021-04-05 13:07:00 +0530

    Card changes

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const withCSS = require('@zeit/next-css');
 module.exports = withCSS({
     publicRuntimeConfig: {
         APP_NAME: 'GeoSpoc CV manager',
-        API_DEVELOPMENT: 'http://localhost:8000/api',
+        API_DEVELOPMENT: 'https://cv-application-manager.herokuapp.com/api',
         API_PRODUCTION: 'https://cv-application-manager.herokuapp.com/api',
-        PRODUCTION: true,
+        PRODUCTION: false,
         // DOMAIN_PRODUCTION: 'https://sharp-shockley-118ffb.netlify.app/',
         DOMAIN_DEVELOPMENT: 'http://localhost:3000',
         GOOGLE_CLIENT_ID:"770856621212-2ogm8kn0p4hv6ispc0upuomnnuilhmkb.apps.googleusercontent.com",
