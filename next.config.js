8c4feb59d2e434ed92e2e1fedaaba60979f90814
tusharswami <tusharswami01@gmail.com>
2021-04-05 11:56:06 +0530

    frontend v2

--- next.config.js
+++ next.config.js
@@ -2,16 +2,14 @@ const withCSS = require('@zeit/next-css');
 
 module.exports = withCSS({
     publicRuntimeConfig: {
-        APP_NAME: 'Arcatron Customer Support Dashboard',
+        APP_NAME: 'GeoSpoc CV manager',
         API_DEVELOPMENT: 'http://localhost:8000/api',
-        API_PRODUCTION: 'https://arcatron-complaints-warranty.herokuapp.com/api',
+        API_PRODUCTION: 'https://cv-application-manager.herokuapp.com//api',
         PRODUCTION: false,
         DOMAIN_PRODUCTION: 'https://sharp-shockley-118ffb.netlify.app/',
         DOMAIN_DEVELOPMENT: 'http://localhost:3000',
         GOOGLE_CLIENT_ID:"770856621212-2ogm8kn0p4hv6ispc0upuomnnuilhmkb.apps.googleusercontent.com",
-        ApplicationApi : 'http://localhost:8000/api/application',
+        ApplicationApi : 'https://cv-application-manager.herokuapp.com/api/application',
         ComplaintAPI : 'http://localhost:8000/api/complaint',
-        WarrantyAPI_Prod : 'https://arcatron-complaints-warranty.herokuapp.com/api/warranty',
-        ComplaintAPI_Prod : 'https://arcatron-complaints-warranty.herokuapp.com/api/complaint'
     }
 }); 
